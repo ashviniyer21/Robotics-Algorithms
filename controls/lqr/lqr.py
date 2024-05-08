70710c383dca58154a3f31ed14cec4728d500a22
Ashvin Iyer <ashvin.iyer@gmail.com>
2024-05-08 14:53:27 -0700

    Adding lqr

---
+++ controls/lqr/lqr.py
@@ -0,0 +1,88 @@
+"""Python file to run and visualize MPC Controller"""
+
+import gymnasium as gym
+import numpy as np
+
+def extrapolate_pos(angles, lens=[0.1, 0.1]):
+    angle0 = angles[0]
+    angle1 = angles[1]
+    x = lens[0] * np.cos(angle0) + lens[1] * np.cos(angle0 + angle1)
+    y = lens[0] * np.sin(angle0) + lens[1] * np.sin(angle0 + angle1)
+    return np.array([x, y])
+
+def extrapolate_angle(goal, lens=[0.1, 0.1]):
+    cos_angle1 = (goal[0] ** 2 + goal[1] ** 2 - lens[0] ** 2 - lens[1] ** 2) / (2 * lens[0] * lens[1])
+    angle1 = np.arccos(cos_angle1)
+    angle0 = np.arctan2(goal[1], goal[0]) - np.arctan2(lens[1] * np.sin(angle1), lens[0] + lens[1] * np.cos(angle1))
+    return np.array([angle0, angle1])
+
+def compute_new_pos(state, vel, dt):
+    return state + vel * dt
+
+def lqr(angles, vel, goal, dt, Q, R, iters=100, lens=np.array([0.1, 0.1]), mass=np.array([0.05, 0.05])):
+    angle_goal = extrapolate_angle(goal)
+    state_goal = np.array([angle_goal[0], angle_goal[1], 0, 0])
+    state = np.array([angles[0], angles[1], vel[0], vel[1]])
+    error = state - state_goal
+    # print(error)
+    # print()
+
+    A = np.array([
+        [1, 0, dt, 0],
+        [0, 1, 0, dt],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1]
+        ])
+    #Moment of inertia
+    I = 1/3 * mass * lens * lens
+
+    B = np.array([
+        [0, 0],
+        [0, 0],
+        [1/I[0] * dt, 0],
+        [0, 1/I[1] * dt]
+    ])
+
+    # P = [None] * (iters + 1)
+    # P[iters] = Q
+    P = Q
+    for _ in range(iters):
+        P = Q + A.T @ P @ A - (A.T @ P @ B) @ np.linalg.pinv(
+            R + B.T @ P @ B) @ (B.T @ P @ A)
+    
+    K = -np.linalg.pinv(R + B.T @ P @ B) @ B.T @ P @ A
+    u = K @ error
+    return u
+
+
+if __name__ == "__main__":
+    DT = 0.02
+    GOAL = 0
+
+    env = gym.make('Reacher-v4', render_mode="human", max_episode_steps=500)
+    observation, info = env.reset()
+    action = [0, 0]
+    for _ in range(1000):
+        observation, reward, terminated, truncated, info = env.step(action)
+        cos0, cos1, sin0, sin1, targetx, targety, vel0, vel1, posx, posy, posz = observation
+        angle0 = np.arctan2(sin0, cos0)
+        angle1 = np.arctan2(sin1, cos1)
+        angles = np.array([angle0, angle1])
+        vels = np.array([vel0, vel1])
+        target = np.array([targetx, targety])
+        Q = np.array([
+            [10, 0, 0, 0],
+            [0, 10, 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]
+        ])
+        R = np.array([
+            [0.1, 0],
+            [0, 0.1]
+        ])
+        action = lqr(angles, vels, target, DT, Q, R)
+        if terminated or truncated:
+            observation, info = env.reset()
+            break
+
+    env.close()
